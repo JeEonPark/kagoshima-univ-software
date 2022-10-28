5c5bb25d0012c367bc018ac55f50c0c82a207537
JeEon Park <ggungtoo@icloud.com>
2022-10-28 15:56:31 +0900

    sw 10 28

--- dragonfly/my.js
+++ dragonfly/my.js
@@ -1,4 +1,76 @@
 $(function() {
+
+    //Graph Class
+
+    var Graph = class {
+        //Constructor
+        constructor(ctx) {
+            this.ctx = ctx;
+        }
+
+        //Clear
+        clear(){
+            ctx.clearRect(0, 0, 640, 480);
+        }
+        
+        //Stroke Ellipse
+        ellipseS(x, y, w, h, a) {
+            this.ctx.beginPath();
+            this.ctx.ellipse(x, y, w, h, a*Math.PI/180, 0, 2*Math.PI);
+            this.ctx.stroke();
+        }
+
+        //Fill Ellipse
+        ellipseF(x, y, w, h, a) {
+            this.ctx.beginPath();
+            this.ctx.ellipse(x, y, w, h, a*Math.PI/180, 0, 2*Math.PI);
+            this.ctx.fill();
+        }
+    }
+
+    //Dragon Fly Class
+    var Dragonfly = class {
+        //Constructor
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.costume = 0;
+        }
+
+        //hirogeteiru
+        drawOpen(gra) {
+            gra.ellipseF(this.x, this.y, 5, 5, 0);
+            gra.ellipseS(this.x, this.y+25, 5, 20, 0);
+            gra.ellipseF(this.x+23, this.y, 20, 5, -15);
+            gra.ellipseF(this.x-23, this.y, 20, 5, 15);
+            gra.ellipseS(this.x+23, this.y+15, 20, 5, 15);
+            gra.ellipseS(this.x-23, this.y+15, 20, 5, -15);
+        }
+
+        //tojiteiru
+        drawClose(gra) {
+            gra.ellipseF(this.x, this.y, 5, 5, 0);
+            gra.ellipseS(this.x, this.y+25, 5, 20, 0);
+            gra.ellipseF(this.x+23, this.y+5, 20, 5, 0);
+            gra.ellipseF(this.x-23, this.y+5, 20, 5, 0);
+            gra.ellipseS(this.x+23, this.y+10, 20, 5, 0);
+            gra.ellipseS(this.x-23, this.y+10, 20, 5, 0);
+        }
+
+        //draw
+        draw(gra) {
+            if(this.costume%2 == 0) {
+                this.drawOpen(gra);
+            } else {
+                this.drawClose(gra);
+            }
+        }
+
+        changeCostume() {
+            this.costume++;
+        }
+    }
+
     //About Message
     var showAboutMessage = function() {
         alert("トンボゲーム\n2022\nSoftware Engineering II");
@@ -9,12 +81,16 @@ $(function() {
 
     var can = $("#can");
     var ctx = can[0].getContext("2d");
+    var gra = new Graph(ctx);
+    var df = new Dragonfly(320, 420);
 
-    ctx.beginPath();
-    ctx.ellipse(100, 100, 5, 5, 0, 0, 2*Math.PI);
-    
-    ctx.fill();
+    var lap = 0;
+    var exec = function() {
+        gra.clear();
+        df.draw(gra);
+        df.changeCostume();
+        lap++;
+    }
 
-    ctx.ellipse(100, 135, 30, 5, 270*Math.PI/180, 0, 2*Math.PI);
-    ctx.stroke();
+    setInterval(exec, 100);
 });
